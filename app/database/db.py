afa55aa8b9f9ded20c8524a0a1dc8f9bdcb2b7a0
Eleftherios Tsourdiou <eleftherios.tsouridou@exness.com>
2025-07-24 12:49:42 +0300

    added tranctions and db

---
+++ app/database/db.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, MetaData
+from databases import Database
+
+DATABASE_URL = "sqlite:///./finance.db"
+
+database = Database(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
